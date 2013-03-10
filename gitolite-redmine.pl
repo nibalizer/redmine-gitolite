49166d4b1488561d2771b6dbfe469b4951ca4f36
Spencer Krum <nibz@cat.pdx.edu>
2013-03-10 07:42:47 -0700

    new function: returnjson

--- gitolite-redmine.pl
+++ gitolite-redmine.pl
@@ -1,35 +1,70 @@
 #!/usr/bin/env perl
 
+#TODO
+# do the curlopt_capath bit from this instead of just turrning off ssl_verify
+# $curl->setopt(CURLOPT_SSL_VERIFYPEER, 0);
+# http://www.tek-tips.com/viewthread.cfm?qid=1460875
+
 
 use strict;
 use warnings;
+use Data::Dumper;               # Perl core module
 use WWW::Curl::Easy;
 use YAML qw(LoadFile);
+use JSON;
 
 my $config = LoadFile('config.yaml');
 print "The API key is ".$config->{'api_key'}."\n";
 print "The Redmine URL is ".$config->{'redmine_url'}."\n";
 
-my $curl = WWW::Curl::Easy->new;
+sub returnjson
+  {
+  my($request_url) = @_;
+  my $curl = WWW::Curl::Easy->new;
+  #my $projects_list_url = $config->{'redmine_url'}."/projects.json\n";
+  #print $projects_list_url;
 
-$curl->setopt(CURLOPT_HEADER,1);
-$curl->setopt(CURLOPT_URL, $config->{'redmine_url'});
+  $curl->setopt(CURLOPT_HEADER,0);
+  $curl->setopt(CURLOPT_SSL_VERIFYPEER, 0);
+  $curl->setopt(CURLOPT_URL, $request_url);
 
-# A filehandle, reference to a scalar or reference to a typeglob can be used here.
-my $response_body;
-$curl->setopt(CURLOPT_WRITEDATA,\$response_body);
+  my @myheaders=(); 
+  $myheaders[0] = "X-Redmine-API-Key: ".$config->{'api_key'}; 
+  $myheaders[1] = "User-Agent: Perl interface for libcURL"; 
 
-# Starts the actual request
-my $retcode = $curl->perform;
+  $curl->setopt(CURLOPT_HTTPHEADER, \@myheaders);
 
-# Looking at the results...
-if ($retcode == 0) {
-        print("Transfer went ok\n");
-        my $response_code = $curl->getinfo(CURLINFO_HTTP_CODE);
-        # judge result and next action based on $response_code
-        print("Received response: $response_body\n");
-} else {
-        # Error code, type of error, error message
-        print("An error happened: $retcode ".$curl->strerror($retcode)." ".$curl->errbuf."\n");
+  # A filehandle, reference to a scalar or reference to a typeglob can be used here.
+  my $response_body;
+  $curl->setopt(CURLOPT_WRITEDATA,\$response_body);
+
+  # Starts the actual request
+  my $retcode = $curl->perform;
+
+  my $curl_response;
+  # Looking at the results...
+  if ($retcode == 0) {
+          print("Transfer went ok\n");
+          my $response_code = $curl->getinfo(CURLINFO_HTTP_CODE);
+          # judge result and next action based on $response_code
+          #print("Received response: $response_body\n");
+          $curl_response = $response_body;
+  } else {
+          # Error code, type of error, error message
+          print("An error happened: $retcode ".$curl->strerror($retcode)." ".$curl->errbuf."\n");
+  }
+  my $perl_scalar = decode_json $curl_response;
+  return $perl_scalar;
 }
+
+
+my $projects_list_url = $config->{'redmine_url'}."/projects.json\n";
+my($projects_list) = &returnjson($projects_list_url);
+
+foreach my $project (@{$projects_list->{'projects'}}){
+  print $project->{'identifier'}.": ".$project->{'id'}."\n";
+
+}
+
+
 
