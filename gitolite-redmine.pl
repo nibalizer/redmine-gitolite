3b6bcf3fe184090e99051701b0f51aa6bfcb236e
Spencer Krum <nibz@cat.pdx.edu>
2013-03-11 03:11:07 -0700

    adding reporer, watcher, editor

--- gitolite-redmine.pl
+++ gitolite-redmine.pl
@@ -12,70 +12,104 @@ use Data::Dumper;               # Perl core module
 use WWW::Curl::Easy;
 use YAML qw(LoadFile);
 use JSON;
+ use feature "switch";
+my $config;
 
-my $config = LoadFile('config.yaml');
-print "The API key is ".$config->{'api_key'}."\n";
-print "The Redmine URL is ".$config->{'redmine_url'}."\n";
+main();
 
-sub returnjson
-  {
-  my($request_url) = @_;
-  my $curl = WWW::Curl::Easy->new;
-  #my $projects_list_url = $config->{'redmine_url'}."/projects.json\n";
-  #print $projects_list_url;
+sub main {
 
-  $curl->setopt(CURLOPT_HEADER,0);
-  $curl->setopt(CURLOPT_SSL_VERIFYPEER, 0);
-  $curl->setopt(CURLOPT_URL, $request_url);
+  $config = LoadFile('config.yaml');
+  print "The API key is ".$config->{'api_key'}."\n";
+  print "The Redmine URL is ".$config->{'redmine_url'}."\n";
 
-  my @myheaders=(); 
-  $myheaders[0] = "X-Redmine-API-Key: ".$config->{'api_key'}; 
-  $myheaders[1] = "User-Agent: Perl interface for libcURL"; 
+  my $projects_list_url = $config->{'redmine_url'}."/projects.json\n";
+  my($projects_list) = &returnjson($projects_list_url);
 
-  $curl->setopt(CURLOPT_HTTPHEADER, \@myheaders);
+  foreach my $project (@{$projects_list->{'projects'}}){
+      print "repo ".$project->{'identifier'}."\n";
+      my $prefix = "/projects/".$project->{'id'};
+      my $members_list_url = $config->{'redmine_url'}."$prefix/memberships.json\n";
+      my($members_list) = &returnjson($members_list_url);
+      each_member ($members_list->{'memberships'});
+      #print Dumper $members_list;
+      #exit 0;
+      print "\n";
 
-  # A filehandle, reference to a scalar or reference to a typeglob can be used here.
-  my $response_body;
-  $curl->setopt(CURLOPT_WRITEDATA,\$response_body);
-
-  # Starts the actual request
-  my $retcode = $curl->perform;
-
-  my $curl_response;
-  # Looking at the results...
-  if ($retcode == 0) {
-    #print("Transfer went ok\n");
-          my $response_code = $curl->getinfo(CURLINFO_HTTP_CODE);
-          # judge result and next action based on $response_code
-          #print("Received response: $response_body\n");
-          $curl_response = $response_body;
-  } else {
-          # Error code, type of error, error message
-          print("An error happened: $retcode ".$curl->strerror($retcode)." ".$curl->errbuf."\n");
   }
-  my $perl_scalar = decode_json $curl_response;
-  return $perl_scalar;
+}
+
+sub returnjson {
+    my($request_url) = @_;
+    my $curl = WWW::Curl::Easy->new;
+    #my $projects_list_url = $config->{'redmine_url'}."/projects.json\n";
+    #print $projects_list_url;
+
+    $curl->setopt(CURLOPT_HEADER,0);
+    $curl->setopt(CURLOPT_SSL_VERIFYPEER, 0);
+    $curl->setopt(CURLOPT_URL, $request_url);
+
+    my @myheaders=(); 
+    $myheaders[0] = "X-Redmine-API-Key: ".$config->{'api_key'}; 
+    $myheaders[1] = "User-Agent: Perl interface for libcURL"; 
+
+    $curl->setopt(CURLOPT_HTTPHEADER, \@myheaders);
+
+    # A filehandle, reference to a scalar or reference to a typeglob can be used here.
+    my $response_body;
+    $curl->setopt(CURLOPT_WRITEDATA,\$response_body);
+
+    # Starts the actual request
+    my $retcode = $curl->perform;
+
+    my $curl_response;
+    # Looking at the results...
+    if ($retcode == 0) {
+        #print("Transfer went ok\n");
+        my $response_code = $curl->getinfo(CURLINFO_HTTP_CODE);
+        # judge result and next action based on $response_code
+        #print("Received response: $response_body\n");
+        $curl_response = $response_body;
+    } else {
+        # Error code, type of error, error message
+        print("An error happened: $retcode ".$curl->strerror($retcode)." ".$curl->errbuf."\n");
+    }
+    my $perl_scalar = decode_json $curl_response;
+    return $perl_scalar;
 }
 
 
-my $projects_list_url = $config->{'redmine_url'}."/projects.json\n";
-my($projects_list) = &returnjson($projects_list_url);
 
-foreach my $project (@{$projects_list->{'projects'}}){
-  #print $project->{'identifier'}.": ".$project->{'id'}."\n";
-  my $prefix = "/projects/".$project->{'id'};
-  my $members_list_url = $config->{'redmine_url'}."$prefix/memberships.json\n";
-  my($members_list) = &returnjson($members_list_url);
-  #print Dumper $members_list;
-  foreach my $member (@{$members_list->{'memberships'}}){
-    if (exists $member->{'user'}) {
-      print $member->{'project'}->{'name'}." ";
-      print $member->{'roles'}[0]->{'name'}." ";
-      print $member->{'user'}->{'name'}."\n";
+
+sub each_member {
+    my($memberships) = @_;
+    foreach my $member (@$memberships){
+        if (exists $member->{'user'}) {
+            print "    ";
+            given ($member->{'roles'}[0]->{'name'}) {
+                when("Manager") {
+                    print "RW+     =   ";
+                }
+                when("Developer") {
+                    print "RW+     =   ";
+                }
+                when("Reporter") {
+                    print "R       =   ";
+                }
+                when("Watcher") {
+                    print "R       =   ";
+                }
+                when("Editor") {
+                    print "RW+     =   ";
+                }
+                when("Viewer") {
+                    print "R       =   ";
+                }
+                default {}
+            }
+            #print $member->{'roles'}[0]->{'name'}." ";
+            print $member->{'user'}->{'name'}."\n";
+        }
     }
-  }
-  #exit 0;
-
-
 }
 
